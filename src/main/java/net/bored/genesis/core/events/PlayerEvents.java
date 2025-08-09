7ac31d1815fe2922a2de85c57c3cf886824ff70a
BoredWasRobbed <bored7160@gmail.com>
2025-08-09 10:09:11 -0400

    Mixins Removed For Now

--- src/main/java/net/bored/genesis/core/events/PlayerEvents.java
+++ src/main/java/net/bored/genesis/core/events/PlayerEvents.java
@@ -3,15 +3,11 @@ package net.bored.genesis.core.events;
 import net.bored.genesis.Genesis;
 import net.bored.genesis.core.capabilities.PowerCapability;
 import net.bored.genesis.core.powers.ISkillPower;
-import net.bored.genesis.mixin.LivingEntityMixin;
-import net.bored.genesis.mixin.accessor.LivingEntityInvoker;
 import net.bored.genesis.network.PacketHandler;
 import net.bored.genesis.network.packets.SyncPowerDataS2CPacket;
-import net.bored.genesis.powers.SpeedsterPower;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -34,30 +30,6 @@ public class PlayerEvents {
                     }
                 });
             });
-        }
-    }
-
-    @SubscribeEvent
-    public static void onUsingItemTick(LivingEntityUseItemEvent.Tick event) {
-        // This logic MUST be on the server to affect the actual item consumption
-        if (event.getEntity() instanceof Player player && !player.level().isClientSide) {
-            if (event.getItem().isEdible()) {
-                player.getCapability(PowerCapability.POWER_MANAGER).ifPresent(powerManager -> {
-                    powerManager.getPower(SpeedsterPower.POWER_ID).ifPresent(power -> {
-                        if (power instanceof SpeedsterPower speedsterPower) {
-                            if (speedsterPower.isSkillUnlocked(SpeedsterPower.SKILL_CALORIE_EFFICIENT)) {
-                                // A normal food item takes 32 ticks. To make it take ~5 ticks (0.25s),
-                                // we need to advance the timer by about 6 extra steps each tick.
-                                for (int i = 0; i < 6; i++) {
-                                    LivingEntityInvoker invoker = (LivingEntityInvoker)(Object) player;
-
-                                    invoker.genesis$updateUsingItem(event.getItem());
-                                }
-                            }
-                        }
-                    });
-                });
-            }
         }
     }
 
